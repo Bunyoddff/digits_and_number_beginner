51521d55ad44c2078188d5fabff1c3a1dabc356f
djumanov <diyorbekjumanov1120@gmail.com>
2022-11-08 11:22:24 +0500

    instructions were changed

---
+++ get_length.py
@@ -0,0 +1,14 @@
+def get_length(num):
+    """
+    A integer is given. Return the number of digits in it.
+
+    Args:
+        num (int): an interger value
+
+    Returns:
+        int: the number of digits
+    """
+    # return number of digits in integer
+    return 
+
+ 
---
+++ get_sum_digits.py
@@ -0,0 +1,12 @@
+def get_sum_digits(num):
+    """
+    A integer is given. Return the sum of digits in it.
+    
+    Args:
+        num (int): an integer value
+    
+    Returns:
+        int: the sum of digits
+    """
+    # return sum of digits in integer
+    return  
